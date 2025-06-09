3ebc82e3cbb3e4e04a11a1ac52572ff33beb1e00
Guilherme Lazari <guilherme_lazari@outlook.com>
2025-06-09 17:18:43 -0300

    Adiciona verificação segura no encodePassword e explicita modificadores nos métodos do listener da classe UserPasswordEncoderListener

--- src/main/groovy/mini/asaas/security/UserPasswordEncoderListener.groovy
+++ src/main/groovy/mini/asaas/security/UserPasswordEncoderListener.groovy
@@ -1,6 +1,7 @@
-package mini.asaas
+package mini.asaas.security
 
 import grails.plugin.springsecurity.SpringSecurityService
+import mini.asaas.user.User
 import org.grails.datastore.mapping.engine.event.AbstractPersistenceEvent
 import org.grails.datastore.mapping.engine.event.PreInsertEvent
 import org.grails.datastore.mapping.engine.event.PreUpdateEvent
@@ -15,25 +16,28 @@ class UserPasswordEncoderListener {
     SpringSecurityService springSecurityService
 
     @Listener(User)
-    void onPreInsertEvent(PreInsertEvent event) {
+    public void onPreInsertEvent(PreInsertEvent event) {
         encodePasswordForEvent(event)
     }
 
     @Listener(User)
-    void onPreUpdateEvent(PreUpdateEvent event) {
+    public void onPreUpdateEvent(PreUpdateEvent event) {
         encodePasswordForEvent(event)
     }
 
     private void encodePasswordForEvent(AbstractPersistenceEvent event) {
         if (event.entityObject instanceof User) {
             User u = event.entityObject as User
-            if (u.password && ((event instanceof  PreInsertEvent) || (event instanceof PreUpdateEvent && u.isDirty('password')))) {
+            if (u.password && ((event instanceof PreInsertEvent) || (event instanceof PreUpdateEvent && u.isDirty('password')))) {
                 event.getEntityAccess().setProperty('password', encodePassword(u.password))
             }
         }
     }
 
     private String encodePassword(String password) {
-        springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+        if (!springSecurityService?.passwordEncoder) {
+            throw new IllegalStateException("Password encoder not configured properly. Cannot encode password.")
+        }
+        return springSecurityService.encodePassword(password)
     }
 }
