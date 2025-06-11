2c4768ad66976bbc50149e23e5583a0714f20ed9
vymartinez <victor.martinez@asaas.com.br>
2025-06-11 16:20:55 -0300

    :sparkles: feat: cria o utils de gson

---
+++ src/main/groovy/mini/asaas/utils/GsonBuilderUtils.groovy
@@ -0,0 +1,20 @@
+package mini.asaas.utils
+
+import com.google.gson.GsonBuilder
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovy.transform.CompileStatic
+
+class GsonBuilderUtils {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+
+    public static <T> T buildClassFromJsonWithType(String json, Class<T> clazz) {
+        String responseAsJson = JsonOutput.toJson(new JsonSlurper().parseText(json))
+        return (T) createGsonBuilder().fromJson(responseAsJson, clazz)
+    }
+
+    private static createGsonBuilder() {
+        return new GsonBuilder().setDateFormat(GsonBuilderUtils.DATE_FORMAT).create()
+    }
+}
