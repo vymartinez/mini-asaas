4713831a450df80bbaf60c7396c71c0db0bd4420
Guilherme Lazari <guilherme_lazari@outlook.com>
2025-06-13 15:08:17 -0300

    Ajusta encode do messages.properties

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
+import org.grails.spring.context.support.ReloadableResourceBundleMessageSource
+
 // Place your Spring DSL code here
 beans = {
+    messageSource(ReloadableResourceBundleMessageSource) {
+        basenames = ['classpath;messages']
+        defaultEncoding = 'UTF-8'
+    }
 }
