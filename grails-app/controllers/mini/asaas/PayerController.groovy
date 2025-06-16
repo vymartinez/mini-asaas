5bf215e204370ed7b19b55514122931f1db922c0
vymartinez <victor.martinez@asaas.com.br>
2025-06-16 16:53:30 -0300

    :sparkles: feat: rota de criação de Payer

---
+++ grails-app/controllers/mini/asaas/PayerController.groovy
@@ -0,0 +1,39 @@
+package mini.asaas
+
+import mini.asaas.adapters.SavePayerAdapter
+import mini.asaas.enums.MessageType
+
+import grails.compiler.GrailsCompileStatic
+import grails.validation.ValidationException
+
+@GrailsCompileStatic
+class PayerController {
+
+    PayerService payerService
+
+    def create() {
+        try {
+            SavePayerAdapter savePayerAdapter = new SavePayerAdapter(params)
+
+            Long currentCustomerId = 1
+            Payer payer = payerService.create(savePayerAdapter, currentCustomerId)
+
+            redirect(url: '/payer/list', model: [payer: payer])
+        } catch (ValidationException e) {
+            flash.message = "Atenção: " + e.errors.allErrors.defaultMessage.join(", ")
+            flash.success = false
+            flash.type = MessageType.ERROR
+            redirect(url: '/payer/create', model: [params: params])
+        } catch (RuntimeException e) {
+            flash.message = e.getMessage()
+            flash.success = false
+            flash.type = MessageType.ERROR
+            redirect(url: '/payer/create', model: [params: params])
+        } catch (Exception e) {
+            flash.message = "Ocorreu um erro interno. Por favor, tente novamente mais tarde."
+            flash.success = false
+            flash.type = MessageType.ERROR
+            redirect(url: '/payer/create', model: [params: params])
+        }
+    }
+}
---
+++ grails-app/services/mini/asaas/PayerService.groovy
@@ -0,0 +1,59 @@
+package mini.asaas
+
+import mini.asaas.adapters.SavePayerAdapter
+import mini.asaas.utils.CpfCnpjUtils
+import mini.asaas.utils.DomainUtils
+import mini.asaas.utils.EmailUtils
+
+import grails.compiler.GrailsCompileStatic
+import grails.gorm.transactions.Transactional
+import grails.validation.ValidationException
+
+@GrailsCompileStatic
+@Transactional
+class PayerService {
+
+    AddressService addressService
+    CustomerService customerService
+
+    public Payer create(SavePayerAdapter savePayerAdapter, Long customerId) {
+        Payer payer = validate(savePayerAdapter)
+
+        if (payer.hasErrors()) throw new ValidationException("Erro ao criar pagador", payer.errors)
+
+        Customer customer = customerService.findById(customerId)
+        Address address = addressService.create(savePayerAdapter.address)
+
+        buildPayer(payer, savePayerAdapter, customer, address)
+
+        payer.save(failOnError: true)
+        return payer
+    }
+
+    private Payer validate(SavePayerAdapter savePayerAdapter) {
+        Payer payer = new Payer()
+
+        if (!savePayerAdapter.name) DomainUtils.addError(payer, "O nome é obrigatório")
+
+        if (!savePayerAdapter.email) DomainUtils.addError(payer, "O email é obrigatório")
+
+        if (savePayerAdapter.email && !EmailUtils.isValid(savePayerAdapter.email)) DomainUtils.addError(payer, "O email informado é inválido")
+
+        if (!savePayerAdapter.cpfCnpj) DomainUtils.addError(payer, "O CPF/CNPJ é obrigatório")
+
+        if (savePayerAdapter.cpfCnpj && !CpfCnpjUtils.validate(savePayerAdapter.cpfCnpj)) DomainUtils.addError(payer, "O CPF/CNPJ informado não é válido")
+
+        if (!savePayerAdapter.cellPhone) DomainUtils.addError(payer, "O celular é obrigatório")
+
+        return payer
+    }
+
+    private buildPayer(Payer payer, SavePayerAdapter savePayerAdapter, Customer customer, Address address) {
+        payer.name = savePayerAdapter.name
+        payer.email = savePayerAdapter.email
+        payer.cpfCnpj = savePayerAdapter.cpfCnpj
+        payer.cellPhone = savePayerAdapter.cellPhone
+        payer.address = address
+        payer.customer = customer
+    }
+}
